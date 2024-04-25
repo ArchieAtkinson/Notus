a0fa570935a37babcb7c58d1a576acb92d114931
Archie Atkinson <archieatkinson97@gmail.com>
2024-04-25 20:00:40 +0000

    fixed token

--- scripts/list_image_tags.sh
+++ scripts/list_image_tags.sh
@@ -2,27 +2,23 @@
 
 # Check if the correct number of arguments was passed
 if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <image-name> <registry-url> <access-token>"
+    echo "Usage: $0 <registry-url> <image-name> <access-token>"
     exit 1
 fi
 
 # Assign the first, second, and third arguments to meaningful variable names
-IMAGE="$1"
-REGISTRY="$2"
+REGISTRY="$1"
+IMAGE="$2"
 TOKEN="$3"
 
 # Get a list of all tags for the image
-RESPONSE=$(curl -s -H "Authorization: Bearer ${TOKEN}" https://${REGISTRY}/v2/${IMAGE}/tags/list)
-
-echo https://${REGISTRY}/v2/${IMAGE}/tags/list
+RESPONSE=$(curl -s -H "Authorization: Bearer $(echo ${TOKEN} | base64)" https://${REGISTRY}/v2/${IMAGE}/tags/list)
 
 # Check if the tags were retrieved successfully
 if [ -z "$RESPONSE" ]; then
     echo "Error: Failed to retrieve the tags."
     exit 1
 fi
-
-echo $RESPONSE 
 
 # Output the list of tags for the image
 echo $RESPONSE | jq -r '.tags[]'
