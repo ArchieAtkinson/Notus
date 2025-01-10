dd3f533a5002ba01a6692043c333c8f3a4a5450c
Archie <archieatkinson97@gmail.com>
2025-01-10 16:26:17 +0000

    Enable Built in SCA  (#11)

--- cmake/Util.cmake
+++ cmake/Util.cmake
@@ -16,6 +16,21 @@ function(ProjectSetup snippets)
     set(SNIPPET_ROOT ${SNIPPET_ROOT} PARENT_SCOPE)
     
     set(SNIPPET ${snippets} PARENT_SCOPE)
+
+    # Use CodeChecker during CI as its slower but covers more-
+    if(DEFINED ENV{GITHUB_ACTIONS})
+        set(ZEPHYR_SCA_VARIANT codechecker PARENT_SCOPE)
+        set(CODECHECKER_PARSE_EXIT_STATUS y PARENT_SCOPE)
+        set(CODECHECKER_ANALYZE_OPTS
+            -i $ENV{NOTUS_ROOT}/codechecker_skip
+            --analyzers=clang-tidy
+            --analyzer-config=clang-tidy:take-config-from-directory=true
+            --quiet
+            --clean
+            PARENT_SCOPE
+        )
+    endif()
+
 endfunction()
 
 function(SetupTarget target_name)
@@ -26,39 +41,37 @@ function(SetupTarget target_name)
         PUBLIC
         ${SYS_COMPILE_FLAGS}
     )
-
-    add_custom_command(TARGET ${target_name} POST_BUILD
+    
+    add_custom_command(
+        TARGET ${target_name} POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E rm -f $ENV{NOTUS_ROOT}/compile_commands.json
         COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_BINARY_DIR}/compile_commands.json $ENV{NOTUS_ROOT}/compile_commands.json    COMMENT "Moving compile_commands.json to $ENV{APPPLICATION_ROOT}"
         VERBATIM
     )
+
+    if("${ZEPHYR_SCA_VARIANT}" STREQUAL "codechecker")
+        # Required for CodeChecker/clang-tidy to not complain
+        add_custom_target(ccClean ALL
+            COMMAND sed -i 's/-fno-freestanding//g' ${CMAKE_BINARY_DIR}/compile_commands.json
+            BYPRODUCTS ${CMAKE_BINARY_DIR}/compile_commands.json
+            COMMENT "Removing -fno-freestanding from compile_commands.json"
+        )
+        add_dependencies(codechecker ccClean)
+    endif()
+    
 endfunction()
 
 function(RunClangdTidy target sources)
-    add_custom_command(TARGET ${target} POST_BUILD
-    COMMAND clangd-tidy
-        -j 8
-        -p ${CMAKE_BINARY_DIR}
-        --clangd-executable clangd-$ENV{LLVM_VERSION}
-        ${sources}
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-    COMMENT "Running clangd-tidy"
-    )
-endfunction()
-
-function(RunClangTidy target)
-    add_custom_target(ClangTidy ALL
-    COMMAND ${CMAKE_COMMAND} -E rm -f ${CMAKE_BINARY_DIR}/clang-tidy-cc/compile_commands.json
-    COMMAND $ENV{NOTUS_ROOT}/scripts/prepare_cc_for_clang_tidy.py 
-        ${CMAKE_BINARY_DIR}/compile_commands.json
-        ${CMAKE_BINARY_DIR}/clang-tidy-cc/compile_commands.json
-    COMMAND run-clang-tidy-17
-        -p ${CMAKE_BINARY_DIR}/clang-tidy-cc
-        -quiet
-    BYPRODUCTS ${CMAKE_BINARY_DIR}/clang-tidy-cc/compile_commands.json
-    DEPENDS ${CMAKE_BINARY_DIR}/compile_commands.json
-    )
-
-    add_dependencies(${target} ClangTidy)
-    add_dependencies(notus_lib ClangTidy)
+    # Use clangd-tidy during local dev as its faster
+    if(NOT DEFINED ENV{GITHUB_ACTIONS})
+        add_custom_command(TARGET ${target} POST_BUILD
+        COMMAND clangd-tidy
+            -j 8
+            -p ${CMAKE_BINARY_DIR}
+            --clangd-executable clangd-$ENV{LLVM_VERSION}
+            ${sources}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+        COMMENT "Running clangd-tidy"
+        )
+    endif()
 endfunction()
